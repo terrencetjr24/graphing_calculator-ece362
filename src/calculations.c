a93dcab6056b5ade39ca86e7bea231d34e174020
terrence <terrencetjr24@gmail.com>
2019-11-01 17:05:40 -0400

    First commit

---
+++ src/calculations.c
@@ -0,0 +1,50 @@
+//Calculation source file
+//Author: Terrence Randall
+//Description: Source file holding the functions of the math operations that will be done
+//(File may later be broken into 2 or 3 files to increase read-ability)
+
+//The basic functions may not be necessary unless we need to manipulate
+//the output to format in some particular way, or for read-ability
+
+//Basic integer functions (if dividing between int and float proves inefficient, delete these)
+int intAdd(int, int);
+int intSub(int, int);
+int intMul(int, int);
+int intDiv(int, int);
+//Basic float functions
+float floatAdd(float, float);
+float floatSub(float, float);
+float floatMul(float, float);
+float floatDiv(float, float);
+
+int intAdd(int a, int b){
+
+}
+
+int intSub(int a, int b){
+
+}
+
+int intMul(int a, int b){
+
+}
+
+int intDiv(int a, int b){
+
+}
+
+float floatAdd(float a, float b){
+
+}
+
+float floatSub(float a, float b){
+
+}
+
+float floatSub(float a, float b){
+
+}
+
+float floatSub(float a, float b){
+
+}
---
+++ src/main.c
@@ -0,0 +1,13 @@
+//Project main source file
+//Group: 021
+//Project: Graphing Calculator w/ Cortex-m0
+//Members: <insert our names>
+//PUIDs: <insert IDs>
+
+#include "stm32f0xx.h"
+#include "stm32f0_discovery.h"
+
+int main(void)
+{
+	for(;;);
+}
